c3e0dbf038138719ec86096f3a3dd3679d2b6373
Kornél Tóth <58789879+TKornel23@users.noreply.github.com>
2022-10-20 18:42:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
                                 </div>
                                 <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
                                     <div class="d-flex flex-row align-items-center mb-1">
-                                        <h4 class="mb-1 me-1">$<?=$row['price']?></h4>
+                                        <h4 class="mb-1 me-1"><?=$row['price']?> Ft</h4>
                                     </div>
                                     <div class="d-flex flex-column mt-4">
                                         <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal<?=$row['id']?>" type="button">Képek megtekintése</button>
