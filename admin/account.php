379d347e575798198347a03944f779f91930ca01
Kornél Tóth <58789879+TKornel23@users.noreply.github.com>
2022-10-20 18:10:38 +0200

    Update account.php

--- admin/account.php
+++ admin/account.php
@@ -3,26 +3,26 @@
     include "header.php"; 
 ?>
 <div class="container mt-2 card p-4">
-    <h3 class="text-center">---- Update Password ----</h3>
+    <h3 class="text-center">---- Jelszó megváltoztatása ----</h3>
     <hr/>
     <form id="update-password" method="POST">
         <div class="row">
             <div class="col-md-4 mt-3">
-                Enter Current Password*
-                <input type="text" class="form-control" placeholder="Current Password" name="curr_pass" id="curr_pass" required>
+                Írja be a jelenlegi jelszót*
+                <input type="text" class="form-control" placeholder="Jelenlegi jelszó" name="curr_pass" id="curr_pass" required>
             </div>
             <div class="col-md-4 mt-3">
-                Enter New Password *
-                <input type="text" class="form-control" placeholder="New Password" name="new_pass" id="new_pass" required>
+                Írja be az új jelszót *
+                <input type="text" class="form-control" placeholder="Új jelszó" name="new_pass" id="new_pass" required>
             </div>
 
             <div class="col-md-4 mt-3">
-                Confirm New Password *
-                <input type="text" class="form-control" placeholder="Confirm New Password" name="confirm_pass" id="confirm_pass" required>
+                Erősítse meg az új jelszót *
+                <input type="text" class="form-control" placeholder="Új jelszó megerősítése" name="confirm_pass" id="confirm_pass" required>
             </div>
         </div>
         <div class="form-group text-right mt-2">
-            <button class="btn btn-md btn-success" type='submit'>Change</button>
+            <button class="btn btn-md btn-success" type='submit'>Jelszó megváltoztatása</button>
         </div>
     </form>
 </div>
