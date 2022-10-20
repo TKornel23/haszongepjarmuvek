25233c9ad34745d3f31be0b253a297e680e22ef8
Kornél Tóth <58789879+TKornel23@users.noreply.github.com>
2022-10-20 18:44:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -142,7 +142,7 @@
                                
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Bezárás</button>
                             </div>
                         </div>
                     </div>
