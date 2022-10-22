8fafb8073865a13900c3a59d2e2c1446bb670e09
Kornél Tóth <58789879+TKornel23@users.noreply.github.com>
2022-10-22 21:29:18 +0200

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -29,7 +29,6 @@
 
     <!-- Library: toastr -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.css">
-    <script src="https://app.simplefileupload.com/buckets/85a8e946f786f22c1faa8af7c7c11930.js"></script>
     <title>Admin | Kezelőfelület</title>
     <style>
         html {
